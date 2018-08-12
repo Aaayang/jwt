6edbfec16c43041251b2120011880ceeece9b138
Aaayang <iamajser@163.com>
2018-08-13 07:39:54 +0800

    01_init

--- 01_init/routes/users.js
+++ 01_init/routes/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/users', function (req, res) {
+router.get('/', function (req, res) {
     res.send('users');
 });
 
